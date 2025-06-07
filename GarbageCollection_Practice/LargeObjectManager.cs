a247dd601df0756f7c697af7203480ddc7c61cec
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-06-07 16:17:09 -0600

    Add project files.

---
+++ GarbageCollection_Practice/LargeObjectManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollection_Practice
+{
+    public class LargeObjectManager
+    {
+        private List<byte[]> _objects;
+
+        private readonly int _memoryLimit;
+
+        public LargeObjectManager(int memoryLimit)
+        {
+            _objects = new List<byte[]>();
+            _memoryLimit = memoryLimit;
+        }
+
+        public void AddLargeObject(int sizeMB)
+        {
+            _objects.Add(new byte[sizeMB * 1024 * 1024]);
+        }
+
+        private long GetCurrentMemoryUsage()
+        {
+            return Process.GetCurrentProcess().PrivateMemorySize64 / (1024 * 1024);
+        }
+
+        public void CheckLimits()
+        {
+            long currentMemory = GetCurrentMemoryUsage();
+            Console.WriteLine($"Current memory usage is {currentMemory} MB.");
+
+            if ( currentMemory >= _memoryLimit )
+            {
+                Console.WriteLine($"Current memory usage exceeds the limit of {_memoryLimit} MB. Cleaning...");
+
+                GC.Collect();
+            }
+
+            long memoryAfter = GetCurrentMemoryUsage();
+            Console.WriteLine($"Memory usage AFTER cleanup is {memoryAfter} MB.");
+        }
+    }
+}
---
+++ GarbageCollection_Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GarbageCollection_Practice
+{
+
+    /*
+     Разработать систему мониторинга использования памяти для большого количества объектов, 
+    где при достижении определенного порога памяти (например, 100 MB) неиспользуемые объекты должны освобождаться вручную.
+
+Создайте класс LargeObjectManager, который управляет созданием больших объектов, 
+    контролирует использование памяти и очищает их, если лимит превышен.
+
+Пример большого объекта:  _objects.Add(new byte[sizeMB * 1024 * 1024]);
+
+    private List<byte[]> _objects = new List<byte[]>();  
+_objects.Add(new byte[sizeMB * 1024 * 1024]);
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LargeObjectManager manager = new LargeObjectManager(100);
+
+            for (int i = 0; i < 100; i++)
+            {
+                manager.AddLargeObject(100);
+                manager.CheckLimits();
+                Thread.Sleep(10);
+            }
+            Console.ReadKey();
+        }
+    }
+}
