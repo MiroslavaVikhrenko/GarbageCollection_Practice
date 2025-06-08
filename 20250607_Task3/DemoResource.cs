8f9918b652d2a2dcd7046ec28b61919abab688fe
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-06-07 18:16:12 -0600

    Practice IDisposable

---
+++ 20250607_Task3/DemoResource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20250607_Task3
+{
+    public class DemoResource : IDisposable
+    {
+        private byte[] largeBuffer; // Simulate large memory usage
+
+        // Constructor
+        public DemoResource()
+        {
+            largeBuffer = new byte[10 * 1024 * 1024]; // Allocate 10 MB buffer
+            Console.WriteLine("DemoResource created with 10 MB buffer.");
+        }
+
+        // Dispose method to clean up the resource
+        public void Dispose()
+        {
+            largeBuffer = null; // Release the buffer
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("DemoResource.Dispose() called. Buffer released.");
+            Console.ResetColor();
+        }
+
+        // Finalizer in case Dispose is not called (not recommended, just for demo)
+        ~DemoResource()
+        {
+            Console.WriteLine("Finalizer called for DemoResource (GC cleanup).");
+        }
+    }
+}
---
+++ 20250607_Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _20250607_Task3
+{
+    /*
+     Создать любой класс и при завершении метода, который создавал объект класса, вызывать метод Dispose.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UseResource(); 
+
+            GC.Collect(); // Force garbage collection 
+            GC.WaitForPendingFinalizers(); // Wait for finalizers to complete
+            Console.ReadKey();
+        }
+
+        // Method to create and dispose of a resource manually
+        static void UseResource()
+        {
+            Console.WriteLine("Entering UseResource()");
+
+            DemoResource resource = new DemoResource(); // Create the resource
+            // Do some work with the resource...
+            Console.WriteLine("Using resource...");
+
+            resource.Dispose(); // Explicitly release memory via Dispose()
+
+            Console.WriteLine("Exiting UseResource()");
+        }
+    }
+}
